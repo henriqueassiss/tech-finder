c778b944fd9dce01285a6c79b0add827bc9c6f0a
henriqueassiss <contato.henriqueassis@outlook.com>
2022-09-13 20:24:18 -0300

    Adding colors

--- src/components/input/Style.ts
+++ src/components/input/Style.ts
@@ -1,5 +1,7 @@
 import { StyleSheet } from 'react-native';
 
+import { colors } from '../../utils';
+
 export const styles = StyleSheet.create({
-	container: {},
+	container: { backgroundColor: colors.white },
 });
---
+++ src/utils/Constants.ts
@@ -0,0 +1,7 @@
+export const colors = {
+	white: 'rgb(255, 255, 255)',
+	black: 'rgb(13, 12, 34)',
+	bright: 'rgb(247, 247, 247)',
+	gray: 'rgb(239, 239, 239)',
+	blue: 'rgb(63, 103, 220)',
+};
