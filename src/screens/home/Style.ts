d16a7b3c47953f4ad3cba9aeb8015528c373c8b8
henriqueassiss <contato.henriqueassis@outlook.com>
2022-09-13 20:24:49 -0300

    Adding home styles

---
+++ src/screens/home/Style.ts
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+
+import { colors } from '../../utils';
+
+export const styles = StyleSheet.create({
+	container: { backgroundColor: colors.bright },
+});
